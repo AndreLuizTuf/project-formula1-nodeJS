88854c7e3f09d9df708004e9e0f6542a1be939d6
André Luiz <andre.luiz.a.f@hotmail.com>
2024-11-04 17:12:22 -0300

    Filter Driver

--- src/server.ts
+++ src/server.ts
@@ -22,8 +22,28 @@ server.get("/teams", async (request, response) => {
 server.get("/drivers", async (request, response) => {
   response.type("application/json").code(200);
 
-  return {drivers};
+  return { drivers };
 });
+
+interface DriverParams {
+  id: string;
+}
+
+server.get<{ Params: DriverParams }>(
+  "/drivers/:id",
+  async (request, response) => {
+    const id = parseInt(request.params.id);
+    const driver = drivers.find((d) => d.id === id);
+
+    if (!driver) {
+      response.type("application/json").code(404);
+      return { error: "Driver not found" };
+    } else {
+      response.type("application/json").code(200);
+      return { driver };
+    }
+  }
+);
 
 server.listen({ port: 3333 }, () => {
   console.log("Server init");
